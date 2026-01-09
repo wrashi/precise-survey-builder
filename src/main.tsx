72f02faaf2d73d537e18d4ad74751afa6f91a86c
Wesley Penner <wesley@mwpenner.com>
2026-01-09 09:16:20 -0700

    Resolving conflicts
    # Conflicts:
    #	src/App.tsx
    #	src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,15 @@ import { Sonner } from '@/components/ui/sonner';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { Toaster } from '@/components/ui/toaster'
+// import { Sonner } from '@/components/ui/sonner'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -16,7 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
-          <Sonner />
+          {/* <Sonner /> */}
           <App />
         </TooltipProvider>
       </QueryClientProvider>
