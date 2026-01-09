4e615fc7790cdc804b515f9262c0762f922bcbf6
wrashi <wrashi@mac.com>
2026-01-09 07:27:31 -0700

    Update vite.config.ts
    Fix base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'precise-survey-builder', // Add this line - use your actual repo name!
+  base: '/precise-survey-builder/', // Add this line - use your actual repo name!
 });
